815888ab5dc7a47b36664d165e73421826337fb4
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-09-06 15:17:35 +0300

    Class attributes and methods lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genres.add(genre)
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artists.add(artist)
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
